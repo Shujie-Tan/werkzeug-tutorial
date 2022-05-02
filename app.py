229cba4422f451f97b54a4c2f552112338d3fc11
tanshujie1 <tanshujie1@jd.com>
2022-05-02 22:55:03 +0800

    chore: config for debug

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-from werkzeug.wrappers import Request, Response
+from shortly.shortly import create_app
 
-def application(environ, start_response):
-    request = Request(environ)
-    text = f"Hello {request.args.get('name', 'World')}!"
-    response = Response(text, mimetype='text/plain')
-    return response(environ, start_response)
+if __name__ == '__main__':
+    from werkzeug.serving import run_simple
+    app = create_app()
+    run_simple('127.0.0.1', 5000, app, use_debugger=True, use_reloader=True)
