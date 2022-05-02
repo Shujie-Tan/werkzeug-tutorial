1d700671360ee4f96c30e88d8caa8273f95baaa2
tanshujie1 <tanshujie1@jd.com>
2022-05-02 23:03:55 +0800

    Step 5: The First View

--- shortly/shortly.py
+++ shortly/shortly.py
@@ -25,6 +25,28 @@ class Shortly(object):
         t = self.jinja_env.get_template(template_name)
         return Response(t.render(context), mimetype='text/html')
 
+    def on_new_url(self, request):
+        error = None
+        url = ''
+        if request.method == 'POST':
+            url = request.form['url']
+            if not is_valid_url(url):
+                error = 'Please enter a valid URL'
+            else:
+                short_id = self.insert_url(url)
+                return redirect(f"/{short_id}+")
+        return self.render_template('new_url.html', error=error, url=url)
+    
+    def insert_url(self, url):
+        short_id = self.redis.get(f'reverse-url:{url}')
+        if short_id is not None:
+            return short_id
+        url_num = self.redis.incr('last-url-id')
+        short_id = base36_encode(url_num)
+        self.redis.set(f'url-target:{short_id}', url)
+        self.redis.set(f'reverse-url:{url}', short_id)
+        return short_id
+
     def dispatch_request(self, request):
         adapter = self.url_map.bind_to_environ(request.environ)
         try:
@@ -53,6 +75,19 @@ def create_app(redis_host='localhost', redis_port=6379, with_static=True):
         })
     return app
 
+def is_valid_url(url):
+    parts = url_parse(url)
+    return parts.scheme in ('http', 'https')
+
+def base36_encode(number):
+    assert number >= 0, 'positive integer required'
+    if number == 0:
+        return '0'
+    base36 = []
+    while number != 0:
+        number, i = divmod(number, 36)
+        base36.append('0123456789abcdefghijklmnopqrstuvwxyz'[i])
+    return ''.join(reversed(base36))
 
 if __name__ == '__main__':
     from werkzeug.serving import run_simple
